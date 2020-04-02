78df14c2dc0e33fd394529e08c52ea3107a813d9
viola2020 <60862445+viola2020@users.noreply.github.com>
2020-04-02 11:08:14 -0400

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,1 +1,28 @@
-<?php phpinfo(); ?>
+<?php 
+if (isset($_POST['submit'])) {
+	require 'phpmailer/PHPMailerAutoload.php';
+	
+	function sendemail($to, $from, $fromName, $message) {
+		$mail = new PHPMailer();
+		$mail->setFrom($from, $fromName);
+		$mail->addAddress($to);
+		$mail->addMessage($message);
+		$mail->Subject = 'Contact Form - Email';
+		$mail->Message = $message;
+		$mail->1stHTML(isHtml:false);
+		
+		return $mail->send();
+		
+	}
+	
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	
+	if(sendemail(to:'violettamaj53@gmail.com', $email, $name, $message));
+	    $msg = 'Email Sent!';
+	else
+		$msg = 'Email Failed!';
+}
+
+?>
