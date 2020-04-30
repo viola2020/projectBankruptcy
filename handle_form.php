007c8cbf06119a0e46b27086689d981818478f34
viola2020 <60862445+viola2020@users.noreply.github.com>
2020-04-30 13:48:33 -0400

    Update handle_form.php

--- handle_form.php
+++ handle_form.php
@@ -15,7 +15,7 @@
  $name = $_POST['name'];
  $email = $_POST['email'];
  $message = $_POST['message'];
- print "<p>Thank you, $name, for your message.</p>
+ print "<p>Thank you, <b>$name</b>, for your message.</p>
  <p>Your message is:<br>
  <br><b>$message</b></p>
  <p> We will respond soon to:
